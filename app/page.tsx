f064fff149015edc46a9106d341814cc9f6f8951
John Mark Tizado <johnmarktizado@gmail.com>
2024-10-21 20:09:13 +0800

    Refactor page layout and add resume builder section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import Footer from "../components/Footer";
+import Layout from "@/components/Layout";
 
 export default function Home() {
   return (
-    <main className="relative flex justify-center items-center flex-col mx-auto overflow-clip">
+    <main className="relative flex justify-center items-center flex-col mx-20 my-20 overflow-clip">
       <div className="max-w-7xl w-full">
-        <h1 className="flex justify-center items-center">Resume Builder</h1>
+        <h1>
+          <span className="text-5xl font-bold">Resume Builder</span>
+        </h1>
+        <p>
+          Create your resume based on Harvard Template. Generate resume in PDF
+          format in just a few clicks.
+        </p>
+        {/* <Personal /> */}
+
+        <Layout />
         <Footer />
       </div>
     </main>
